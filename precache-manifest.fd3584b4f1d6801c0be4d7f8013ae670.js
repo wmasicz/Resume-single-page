35b6bb24cb1d66d3b333a42edc391ef08c00fea6
Wojciech Masicz <wmasicz@gmail.com>
2020-03-07 20:59:34 +0100

    Updates

---
+++ precache-manifest.fd3584b4f1d6801c0be4d7f8013ae670.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df691195ea3a6b93ff9991254726501b",
+    "url": "/resume-single-page/index.html"
+  },
+  {
+    "revision": "765ce6f07b7d9a1e2abe",
+    "url": "/resume-single-page/static/css/2.cac4d284.chunk.css"
+  },
+  {
+    "revision": "459867d85cf033525cd0",
+    "url": "/resume-single-page/static/css/main.60a7ef5c.chunk.css"
+  },
+  {
+    "revision": "765ce6f07b7d9a1e2abe",
+    "url": "/resume-single-page/static/js/2.cc5e4217.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/resume-single-page/static/js/2.cc5e4217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459867d85cf033525cd0",
+    "url": "/resume-single-page/static/js/main.0f46e316.chunk.js"
+  },
+  {
+    "revision": "606d8fb35ec25313673d",
+    "url": "/resume-single-page/static/js/runtime-main.ebc299fe.js"
+  },
+  {
+    "revision": "6a4830bb8d5cb5f89a608f46150025c7",
+    "url": "/resume-single-page/static/media/avatar.6a4830bb.png"
+  },
+  {
+    "revision": "2b63203f1e90ea80533be6bd7c0e98a3",
+    "url": "/resume-single-page/static/media/pic2.2b63203f.jpg"
+  },
+  {
+    "revision": "be01f9a5943c2cf2838e900000e78231",
+    "url": "/resume-single-page/static/media/pic3.be01f9a5.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/resume-single-page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/resume-single-page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/resume-single-page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/resume-single-page/static/media/slick.f97e3bbf.svg"
+  }
+]);
